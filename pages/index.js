4d3fe034b5c30ee76fc53be56534e6375dcac167
ooofer-landing <92044431+ooofer-landing@users.noreply.github.com>
2021-10-07 08:40:14 +0200

    Fix background alignment

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main className="h-screen flex items-center justify-center ">
-        <div>
+        <div className="text-center">
           <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-7xl">
             <span className="font-light">OOO</span>FER
           </h1>
